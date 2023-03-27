c04079c85f406676280bb7b9117033b169e01fcb
lzhang714 <longzhang2008@gmail.com>
2023-03-27 18:51:16 -0400

    Update plo_interface.f90

--- plo_interface.f90
+++ plo_interface.f90
@@ -1,9 +1,9 @@
 !
 ! Reads Bloch hamiltonian and projectors on correlated subspace from input file. 
 !
-! Long Zhang created, most recently modified 2017
+! L.Zhang: created, then most recently modified 2017
 !
-!
+
   subroutine plo_read_projectors()
 
     use constants
